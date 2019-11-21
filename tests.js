8455a78a6cb0dd222adcbee072c9225e13ed9699
Mary-Mik <kirymari@yandex.ru>
2019-11-21 15:27:15 +0300

    Calculator JS
    Object Calculator JS

---
+++ tests.js
@@ -0,0 +1,34 @@
+﻿"use strict"
+
+describe("calculator", function () {
+
+    describe("read", function () {
+        it("Читает и записывает первое число", function () {
+            let expected = 10;
+            assert.equal(calculator.firstNumber, expected);
+        });
+        it("Читает и записывает второе число", function () {
+            let expected = 10;
+            assert.equal(calculator.secondNumber, expected);
+        });
+    });
+
+    describe("sum", function () {
+
+       it("складывает два числа", function () {
+            let expected = 10 + 10;
+            assert.equal(calculator.sum(), expected);
+       });
+        
+    });
+
+    describe("mul", function () {
+
+
+        it("умножает два числа", function () {
+            let expected = 10*10;
+            assert.equal(calculator.mul(), expected);
+        });
+    });
+
+});
